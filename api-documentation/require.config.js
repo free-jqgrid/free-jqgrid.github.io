bc76a598c5139e512d2568df4a8fd24615317289
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2016-07-22 17:33:05 +0200

    fix dns-prefetch and preconnect to res.cloudinary.com

--- api-documentation/require.config.js
+++ api-documentation/require.config.js
@@ -43,20 +43,20 @@ define([
 			datatype: "json",
 			colModel: [
 				{ name: "base", hidden: true },
-				{ name: "fullName", width: 300,
+				{ name: "fullName", width: 282,
 					jsonmap: function (obj) {
 						return obj.base + "." + obj.name;
 					} },
-				{ name: "name",
+				{ name: "name", width: 171,
 					cellattr: function (rowId, cellValue, rawObject, cm, rdata) {
 						return "title='" + rdata.base + "." + rdata.name + "'";
 					} },
-				{ name: "methodType", stype: "select",
+				{ name: "methodType", width: 89, stype: "select",
 					searchoptions: {
 						sopt: ["eq", "ne"],
 						value: ":Any;standard:standard;static:static;static-this-grid:static-this-grid"
 					} },
-				{ name: "category", width: 300 }
+				{ name: "category", width: 196 }
 			],
 			rowNum: 10,
 			pager: true,
